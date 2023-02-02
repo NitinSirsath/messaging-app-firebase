fc2108a5bef1c926037ec580da442a8105106f95
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2023-02-02 15:22:16 +0530

    added pages
    -added home, register and login pages

---
+++ src/pages/Login.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+type Props = {}
+
+const Login = (props: Props) => {
+  return (
+    <div className="formContainer">
+    <div className="formWrapper">
+      <span className="logo">Lama Chat</span>
+      <span className="title">Login</span>
+      <form >
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button>Sign in</button>
+        {/* {err && <span>Something went wrong</span>} */}
+      </form>
+      {/* <p>You don't have an account? <Link to="/register">Register</Link></p> */}
+    </div>
+  </div>
+  )
+}
+
+export default Login
