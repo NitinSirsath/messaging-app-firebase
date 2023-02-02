dd9a35f8bc22bef7c02db2ee876d8cf47204d769
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2023-02-02 23:53:55 +0530

    update chat

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useContext } from "react";
+import Cam from "../assets/cam.png";
+import Add from "../assets/add.png";
+import More from "../assets/more.png";
+import Messages from "./Messages";
+import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";import React from 'react'
 
 type Props = {}
 
 const Chat = (props: Props) => {
-  return (
-    <div>Chat</div>
+    const { data } = useContext(ChatContext);
+
+    return (
+      <div className="chat">
+        <div className="chatInfo">
+          <span>{data.user?.displayName}</span>
+          <div className="chatIcons">
+            <img src={Cam} alt="" />
+            <img src={Add} alt="" />
+            <img src={More} alt="" />
+          </div>
+        </div>
+        <Messages />
+        <Input/>
+      </div>
   )
 }
 
