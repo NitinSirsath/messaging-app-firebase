420b6ca6481fc1115b051693bff4cf8f11a02a5d
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2023-02-02 22:43:14 +0530

    update app
    -update app.tsx
    -setup contextAPi

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import './App.css'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import './style.scss'
-
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import "./style.scss";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from './context/AuthContext'
 
 function App() {
 
+  const { currentUser } = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children
+  };
+
   return (
     <div className="App">
-     <Register />
-     {/* <Login /> */}
+    <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
